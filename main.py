3225b94ddeba109650654f8ebda5f2c9309968c1
橋本翔 <kakeru@MacBook-Pro-3.local>
2020-06-22 17:42:30 +0900

    [clean]

--- main.py
+++ main.py
@@ -32,15 +32,15 @@ class BRDF:
         self.alpha = roughness ** 2
         self.glow = False
     def diffuse(self):
-        return self.basecolor/np.pi
+        return np.clip(self.basecolor/np.pi,0,1)
     def D(self,n,h):
-        return np.clip(self.alpha**2/(np.pi* (np.dot(n,h)**2 * (self.alpha**2-1) + 1)**2),0,1000)
+        return np.clip(self.alpha**2/(np.pi* (np.dot(n,h)**2 * (self.alpha**2-1) + 1)**2),0,1)
     def V(self,n,v,l):
         al = np.dot(n,v)*np.sqrt(np.dot(n,l)**2 * (1-self.alpha**2) +  self.alpha**2)
         av = np.dot(n,l)*np.sqrt(np.dot(n,v)**2 * (1-self.alpha**2) +  self.alpha**2)
-        return 0.5/(al+av)
+        return np.clip(0.5/(al+av),0,1)
     def F(self,l,h):
-        return self.f0 + (1-self.f0)*((1-np.dot(l,h))**5)
+        return np.clip(self.f0 + (1-self.f0)*((1-np.dot(l,h))**5),0,1)
     
     def brdf(self,n,v,l):
         h = (v+l)/np.linalg.norm(v+l)
@@ -88,11 +88,10 @@ class Object:
         buf = np.array(buf)
         return buf/np.linalg.norm(buf)
 
-    def mat_vecs(self,x,cam_pos,lay_vec):
+    def mat_vecs(self,x,cam_pos):
         n = self.norm(x)
         v = (cam_pos-x)/np.linalg.norm(cam_pos-x)
-        l = lay_vec
-        return n,v,l
+        return n,v
 
     def set_material(self,mat):
         self.mat = mat
@@ -127,35 +126,37 @@ class Box(Object):
 class Scene:
     def __init__(self,resolution=256):
         obj1 = Sphere(1.5,[0,0,20])
-        obj2 = Box(np.array([5,5,5]),np.array([0,0,40]))
+        obj2 = Box(np.array([5,1,5]),np.array([0,-3,20]))
         # obj1.set_material(DiffuseMaterial(np.array([255,0,0])))
         obj2.set_material(BRDF(np.array([1,0,0]),np.array([0.6,0.6,0.6]),0.1))
+        obj1.set_material(BRDF(np.array([0,1,0]),np.array([0.6,0.6,0.6]),0.1))
         # obj2.set_material(BRDF(np.array([1,1,0]),np.array([0.02,0.02,0.02]),0.1))
-        obj1.set_material(GlowMaterial(np.array([0,1,1]),2))
+        # obj1.set_material(GlowMaterial(np.array([0,1,1]),2))
         # obj1.set_material(BasicMaterial(1,np.array([1,0,0]),np.array([1,1,1]),30))
         self.objs = [obj1,obj2]
-        self.lights = [DirectionalLight(np.array([1,-1,1]),np.array([1,1,1]))]
+        self.lights = [DirectionalLight(np.array([1,-1,1]),np.array([5,5,5]))]
         self.img = np.array([[[0,0,0] for i in range(resolution)] for j in range(resolution)]).astype(np.uint8)
         self.step = 2/resolution
-        self.maxdis = 10
-        self.mindis = 0.01
+        self.maxdis = 100
+        self.mindis = 0.001
         self.resolutuon = resolution
         self.cam = None
         self.arg_ij = itertools.product(range(self.resolutuon),range(self.resolutuon))
     
     def render(self,camera):
         self.cam = camera
-        with Pool(8) as p:
-            ret = p.starmap(self.marching, self.arg_ij)
-        with Pool(8) as p:
-            ret2 = p.starmap(self.hitcolor,ret)
-        for r in ret2:
-            self.img[r[0]][r[1]][0] += np.clip(r[2][0],0,1)*255
-            self.img[r[0]][r[1]][1] += np.clip(r[2][1],0,1)*255
-            self.img[r[0]][r[1]][2] += np.clip(r[2][2],0,1)*255
+        # with Pool(8) as p:
+        #     ret = p.starmap(self.marching, self.arg_ij)
+        # with Pool(8) as p:
+        #     ret2 = p.starmap(self.hitcolor,ret)
+        # for r in ret2:
+        #     self.img[r[0]][r[1]][0] += np.clip(r[2][0],0,1)*255
+        #     self.img[r[0]][r[1]][1] += np.clip(r[2][1],0,1)*255
+        #     self.img[r[0]][r[1]][2] += np.clip(r[2][2],0,1)*255
 
         with Pool(8) as p:
-            ret3 = p.starmap(self.glowcolor,ret)
+            ret3 = p.starmap(self.cast_ray,self.arg_ij)
+        # print(ret3)
         for r in ret3:
             self.img[r[0]][r[1]][0] += np.clip(r[2][0],0,1)*255
             self.img[r[0]][r[1]][1] += np.clip(r[2][1],0,1)*255
@@ -165,67 +166,115 @@ class Scene:
         tmp = Image.fromarray(self.img)
         tmp.save("./res.png")
 
-    def hitcolor(self,i,j,hitflag,p,x,shortest2objs):
-        if hitflag == -1:
-            return [i,j,np.array([0,0,0])]
-        else:
-            obj = self.objs[hitflag]
-            if not obj.mat.glow:
-                buf = np.array([0,0,0])
-                for light in self.lights:
-                    lv,lpow = light.lighting(x)
-                    n,v,l = obj.mat_vecs(x,p,lv)
-                    brdf = obj.mat.brdf(n,v,l)
-                    lcos = np.clip(np.dot(l,n),0,1)
-                    buf = buf + np.clip(lcos*lpow*brdf,0,1)
-                buf = buf+obj.mat.diffuse()*np.array([0.4,0.4,0.4])
-                return [i,j,buf]
-            else:
-                return [i,j,obj.mat.glowcolor]
-
-    def glowcolor(self,i,j,hitflag,p,x,shortest2objs):
-        if hitflag != -1:
-            hoge = self.cam.pos-self.objs[hitflag].pos
-            obstdist = np.sqrt(np.dot(hoge,hoge))
-        else:
-            obstdist = 1e9
-
-        buf = np.array([0,0,0])
-        for ind in range(len(self.objs)):
-            if self.objs[ind].mat.glow:
-                hoge = self.cam.pos-self.objs[ind].pos
-                gdist = np.sqrt(np.dot(hoge,hoge))
-                if gdist < obstdist:
-                    buf = buf + self.objs[ind].mat.glowcolor*((shortest2objs[ind]+1.5)**(-self.objs[ind].mat.alpha))
-        return [i,j,buf]
-
-
-            
-
-
-    def marching(self,i,j):
-        tx,ty = self.step*j-1,-self.step*i+1
-        ray = self.cam.ray(tx,ty)
+    def ray_marching(self,origin,ray):
         totdis = 0
-        maxdis = 100
-        mindis = 0.001
         hitflag = -1
-        p = self.cam.pos
         numobj = len(self.objs)
         shortest2objs = [1e9 for hoge in range(numobj)]
-        while totdis < maxdis:
-            rayhead = p+totdis*ray
+
+        while totdis < self.maxdis:
+            rayhead = origin + ray*totdis
             ds = [o.dist(rayhead) for o in self.objs]
             for hoge in range(numobj):
                 shortest2objs[hoge] = min(shortest2objs[hoge],ds[hoge])
             near_ind = np.argmin(ds)
             d = ds[near_ind]
-            if abs(d) < mindis:
+            if abs(d) < self.mindis:
                 hitflag = near_ind
                 break
             else:
                 totdis += d
-        return [i,j,hitflag,p,p+totdis*ray,shortest2objs]
+        return hitflag,totdis,shortest2objs
+    
+    def is_hit(self,origin,ray):
+        totdis = 0
+        hitflag = -1
+        numobj = len(self.objs)
+
+        while totdis < self.maxdis:
+            rayhead = origin + ray*totdis
+            ds = [o.dist(rayhead) for o in self.objs]
+            near_ind = np.argmin(ds)
+            d = ds[near_ind]
+            if abs(d) < self.mindis:
+                hitflag = near_ind
+                break
+            else:
+                totdis += d
+        return hitflag
+
+    def cast_ray(self,i,j):
+        #i,jは一旦置いておく
+        #rayのcolorは((1-shadow)*light+AO)*BRDF + glow
+        # lightは照明光と反射先のカラー。反射光は鏡面反射部分にしかかからない？→いやかかっていいはず。
+        # glowは照明扱いしなくちゃホントはあかんけど、減衰が強いってことで無かったことに…(だるい)
+        
+        tx,ty = self.step*j-1,-self.step*i+1
+        ray = self.cam.ray(tx,ty)
+        origin = self.cam.pos
+        totcol = np.array([0,0,0])
+        reflection_max = 1
+        for loop in range(reflection_max):
+            hitflag,totdis,shortest2objs = self.ray_marching(origin,ray)
+            if hitflag == -1:
+                break
+            else:
+                hitpos = origin + ray*totdis
+                n,v = self.objs[hitflag].mat_vecs(hitpos,origin)
+                hitpos = hitpos + n*0.02
+                # refv = self.reflect(v,n)
+                for light in self.lights:
+                    lv,lpow = light.lighting(hitpos)
+                    totcol = totcol + self.hcolor(hitflag,hitpos,n,v,lv,lpow) #BRDF
+                totcol = totcol + self.gcolor(hitflag,origin,shortest2objs) #GLOW
+        return [i,j,totcol]
+        
+    
+    def reflect(self,v,n):
+        # vは視線ベクトルなど物体から視点方向に生える向き
+        return -v+2*np.dot(v,n)*n/np.linalg.norm(-v+2*np.dot(v,n)*n)
+
+    def hcolor(self,hitflag,hitpos,n,v,l,lpow):
+        is_shadow = self.is_shadow(hitpos,l)
+        if is_shadow:
+            lp = lpow*0.2
+        else:
+            lp = lpow*np.clip(np.dot(l,n),0.2,1)
+        tmp = np.array([0,0,0])
+        if hitflag != -1:
+            obj = self.objs[hitflag]
+            if not obj.mat.glow:
+                brdf = np.clip(obj.mat.brdf(n,v,l),0,1)
+                tmp = tmp + np.clip(lp*brdf,0,1)
+            else:
+                tmp = tmp + obj.mat.glowcolor
+        return tmp
+
+    def gcolor(self,hitflag,origin,shortest2objs):
+        tmp = np.array([0,0,0])
+        if hitflag != -1:
+            hoge = origin-self.objs[hitflag].pos
+            obstdist = np.sqrt(np.dot(hoge,hoge))
+        else:
+            obstdist = 1e9
+
+        for ind in range(len(self.objs)):
+            if self.objs[ind].mat.glow:
+                hoge = origin-self.objs[ind].pos
+                gdist = np.sqrt(np.dot(hoge,hoge))
+                if gdist < obstdist:
+                    tmp = tmp + self.objs[ind].mat.glowcolor*((shortest2objs[ind]+1.5)**(-self.objs[ind].mat.alpha))
+
+        return tmp
+    
+    def is_shadow(self,origin,l):
+        hitflag = self.is_hit(origin,l)
+        if hitflag == -1:
+            return False
+        else:
+            return True
+
+
 
 sc = Scene()
 cam = Camera()
